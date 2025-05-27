059cfd98a775625a2b492eb53702534eb6ff8098
Acloyer <Rafig12332@gmail.com>
2025-05-27 01:20:18 -0700

    feat: add Task struct and timestamp utility

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <vector>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+struct Task {
+    int id;
+    string description;
+    string status; // todo, in-progress, done
+    string createdAt;
+    string updatedAt;
+};
+
+vector<Task> tasks;
+const string FILE_NAME = "tasks.json";
+
+string getCurrentTimestamp() {
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    string str(dt);
+    str.pop_back(); // remove newline
+    return str;
+}
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
     for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+         std::cout << "i = " << i << std::endl;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
-}
+    }
